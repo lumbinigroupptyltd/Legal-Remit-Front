67bc3e03cd13a75caf0f8ce229cf0481d96c55a1
ganeshbhatt123 <ganesh.bhatt@legalremit.com>
2024-06-05 15:15:38 +0000

    build testing...

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
@@ -16,11 +16,11 @@ export default defineConfig({
     },
   },
   optimizeDeps: {
-    include: ['export-to-csv', 'jspdf-autotable', 'jspdf-autotable', 'material-react-table'],
+    include: ['export-to-csv', 'jspdf-autotable', 'material-react-table'],
   },
   build: {
     rollupOptions: {
-      external: ['export-to-csv', 'jspdf-autotable', 'jspdf-autotable', 'material-react-table'],
+      external: ['export-to-csv', 'jspdf-autotable', 'material-react-table'],
     },
   },
 });
